0094b91e61c3342e770b91e19ca138d4746d54dc
HidenLee <hyundo1995@gmail.com>
2023-10-24 02:41:29 +0900

    [FE] Test: Navigation sample 구현

--- app/src/main/java/com/ssafy/instargram/ui/NavGraph.kt
+++ app/src/main/java/com/ssafy/instargram/ui/NavGraph.kt
@@ -1,2 +1,47 @@
 package com.ssafy.instargram.ui
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun NavGraph(
+    navController: NavHostController = rememberAppNavigationController(),
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route,
+        modifier = modifier
+    ) {
+        composable(route = Screen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+        composable(route = Screen.Example.route) {
+            ExampleScreen(navController = navController)
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen(navController: NavController) {
+    Button(onClick = { navController.navigate("Example") }) {
+        Text(text = "Home")
+    }
+}
+
+@Composable
+fun ExampleScreen(navController: NavController) {
+    Button(onClick = { navController.navigate("Home") }) {
+        Text(text = "Example")
+    }
+}
+
+
+
--- app/src/main/java/com/ssafy/instargram/ui/Navigation.kt
+++ app/src/main/java/com/ssafy/instargram/ui/Navigation.kt
@@ -1,2 +1,15 @@
 package com.ssafy.instargram.ui
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Example : Screen("example")
+}
+
+@Composable
+fun rememberAppNavigationController(): NavHostController {
+    return rememberNavController()
+}
