cf29e3e3f7ccedf6e48027725db7302ef5206cc6
HidenLee <hyundo1995@gmail.com>
2023-10-24 12:37:06 +0900

    [FE] Fix: 카카오 로그인 미작동 이슈, AndroidManifest.xml에 필요한 Activity 작성으로 해결

--- app/src/main/java/com/ssafy/stellargram/ui/screen/kakao/KakaoViewModel.kt
+++ app/src/main/java/com/ssafy/stellargram/ui/screen/kakao/KakaoViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
 import com.kakao.sdk.auth.model.OAuthToken
+import com.ssafy.stellargram.ui.Screen
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -14,12 +15,12 @@ class KakaoViewModel @Inject constructor() : ViewModel() {
     fun handlelogin(token: OAuthToken?, error: Throwable?, navController: NavController) {
         if (error != null) {
             Log.e(TAG, "로그인 실패", error)
-            navController.navigate("landing")
+            navController.navigate(Screen.Landing.route)
             // Handle login failure here
         } else if (token != null) {
             Log.i(TAG, "로그인 성공 ${token.accessToken}")
             // After successful login, request user information
-            navController.navigate("home")
+            navController.navigate(Screen.Home.route)
 //            retrieveUserInfo(token, navController)
         }
     }
