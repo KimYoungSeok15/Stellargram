b70425e1edc1c70fbeff91b29d4ffa83094fffda
ppsrac <ppsracchriskim@gmail.com>
2023-10-27 08:41:09 +0900

    [BE] Feat: Add feign configurations

---
+++ backend/observesite/src/main/java/com/instargram101/global/config/OpenFeignConfig.java
@@ -0,0 +1,27 @@
+package com.instargram101.global.config;
+
+import feign.Request;
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.apache.commons.lang.ArrayUtils;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableFeignClients("com.instargram101.global")
+public class OpenFeignConfig {
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return requestTemplate -> {
+            if(ArrayUtils.isEmpty(requestTemplate.body()) && !isGetOrDelete(requestTemplate)) {
+                requestTemplate.body("{}");
+            }
+        };
+    }
+
+    private boolean isGetOrDelete(RequestTemplate requestTemplate) {
+        return Request.HttpMethod.GET.name().equals(requestTemplate.method())
+            || Request.HttpMethod.DELETE.name().equals(requestTemplate.method());
+    }
+}
