e91946a40940702dac64098cedac6b0dbfabcf4c
ppsrac <ppsracchriskim@gmail.com>
2023-10-27 08:47:22 +0900

    [BE] Feat: Add mapstruct dependencies

--- backend/observesite/src/main/java/com/instargram101/observesite/dto/request/ObserveSiteInfoRequest.java
+++ backend/observesite/src/main/java/com/instargram101/observesite/dto/request/ObserveSiteInfoRequest.java
@@ -1,4 +1,13 @@
 package com.instargram101.observesite.dto.request;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class ObserveSiteInfoRequest {
 }
