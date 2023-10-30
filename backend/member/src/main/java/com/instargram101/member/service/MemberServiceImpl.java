e2db4d94e42de6a7842503f93b06effe794f864f
오정빈 <ohjb93@gmail.com>
2023-10-30 09:16:36 +0900

    [member] 회원가입 기능 구협

--- backend/member/src/main/java/com/instargram101/member/service/MemberServiceImpl.java
+++ backend/member/src/main/java/com/instargram101/member/service/MemberServiceImpl.java
@@ -1,5 +1,6 @@
 package com.instargram101.member.service;
 
+import com.instargram101.member.dto.request.SignMemberRequestDto;
 import com.instargram101.member.entity.Member;
 import com.instargram101.member.repoository.MemberRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,5 +20,22 @@ public class MemberServiceImpl implements MemberService {
         Optional<Member> member = memberRepository.findByMemberId(memberId);
         return !member.isEmpty();
 
+    }
+
+    public Member createMember(Long memberId, SignMemberRequestDto request) {
+        String nickname = request.getNickname();
+        String profileImageUrl = request.getProfileImageUrl();
+
+        Member member = Member.builder()
+                .memberId(memberId)
+                .nickname(nickname)
+                .profileImageUrl(profileImageUrl)
+                .activated(true)
+                .followCount(0L)
+                .followingCount(0L)
+                .cardCount(0L)
+                .build();
+
+        return memberRepository.save(member);
     }
 }
