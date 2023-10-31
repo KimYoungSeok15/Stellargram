91dd94129a4882000893b83d728e1e9025002082
HidenLee <hyundo1995@gmail.com>
2023-10-31 10:02:40 +0900

    [FE] Design: BaseFrame Bottom navigation icon 순서 변경, 배경색 투명화

--- frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/base/BaseFrame.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/base/BaseFrame.kt
@@ -1,29 +1,35 @@
 package com.ssafy.stellargram.ui.screen.base
 
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.ssafy.stellargram.ui.Screen
+@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BaseFrame(
-    navController: NavController,
+    navController: NavController = rememberNavController(),
     content: @Composable BoxScope.() -> Unit = { example() })
 {
     Scaffold(
@@ -37,20 +43,25 @@ fun BaseFrame(
             content = {
                 val items = listOf(
                     Screen.Home,
-                    Screen.GoogleMap,
-                    Screen.MyPage,
+                    Screen.SkyMap,
                     Screen.CameraX,
-                    Screen.SkyMap
+                    Screen.GoogleMap,
+                    Screen.MyPage
                 )
                 items.forEach{
                     NavigationBarItem(
                         selected =  navController.currentDestination?.route == it.route ,
                         onClick = { navController.navigate(it.route) },
-                        icon = { Icon(painter = painterResource(id = it.icon), contentDescription = it.title ) },
-                        label = { it.title }
+                        icon = { Icon(painter = painterResource(id = it.icon), contentDescription = it.title)},
+                        label = { it.title },
+                        modifier = Modifier
+                            .padding(20.dp)
+                            .width(20.dp)
+                            .height(36.dp)
                     )
                 }
-            }
+            },
+            containerColor = Color.Transparent
         ) }
     ) {
         Box(modifier = Modifier.padding(it)){
