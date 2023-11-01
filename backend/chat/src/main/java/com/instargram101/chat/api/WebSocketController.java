32ca7efa0b2265f44cda8581de3bd65de860c11f
Sohee-Kim-ln <livre6491@gmail.com>
2023-11-01 12:46:34 +0900

    Feat: websocket 요청 컨트롤러 구현

--- backend/chat/src/main/java/com/instargram101/chat/api/WebSocketController.java
+++ backend/chat/src/main/java/com/instargram101/chat/api/WebSocketController.java
@@ -1,14 +1,56 @@
 package com.instargram101.chat.api;
 
+import com.instargram101.chat.entity.ChatMessage;
+import com.instargram101.chat.service.MessageService;
+import com.instargram101.chat.service.RedisPublisher;
+import com.instargram101.chat.service.RedisSubscriber;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
+@Slf4j
+@RequiredArgsConstructor
 @Controller
 public class WebSocketController {
-    // 웹소켓 연결
+    public final MessageService messageService;
 
-    // 특정 채팅방 구독
-    
+    public final RedisMessageListenerContainer ListenerContainer;
+    public final RedisPublisher redisPublisher;
+    public final RedisSubscriber redisSubscriber;
+    public final SimpMessagingTemplate messageTemplate;
+    public final SimpMessageSendingOperations sendingOperations;
+
     // 특정 채팅방에 메세지 발행
-    
-    // 웹소켓 연결 종료
+    @MessageMapping("/room/{chatRoomId}")
+    public void publishMessage(@DestinationVariable("chatRoomId") Long roomId, ChatMessage message) {
+        log.info("publishted at " + roomId.longValue());
+        System.out.println(message);
+//        messageService.sendMessageToRoom(roomId,message)
+        message.setRoomId(roomId);
+        messageService.sendMessageToRoom(roomId,message);
+
+        // 메세지 받아서 레디스에 퍼블리시하기
+//                redisPublisher.publishMessage(roomId,message);
+
+//        ChatMessage newMessage = ChatMessage.builder()
+//                .roomId(roomId)
+//                .content("test")
+//                .build();
+//        System.out.println(newMessage.toString());
+//        messageTemplate.convertAndSend("/subscribe/room/" + roomId, message);
+    }
+
+    // 테스트
+    @MessageMapping("/test")
+    public void forTest() {
+        log.info("WebSocket Test");
+        System.out.println("WebSocket Test println");
+    }
 }
