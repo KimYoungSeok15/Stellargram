455248b3afcd3522705da3cd60048c36551fe44c
HyundoLee <hyundo1995@gmail.com>
2023-11-06 18:04:37 +0900

    [FE] Fix: Schema difference 이슈 해결

--- frontend/app/src/main/java/com/ssafy/stellargram/data/db/dao/StarDAO.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/data/db/dao/StarDAO.kt
@@ -1,5 +1,6 @@
 package com.ssafy.stellargram.data.db.dao
 
+import androidx.compose.ui.input.pointer.PointerId
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
@@ -11,8 +12,11 @@ import kotlinx.coroutines.flow.Flow
 interface StarDAO {
     @Insert(onConflict = OnConflictStrategy.ABORT)
     suspend fun addStar(item: Star)
-    @Query(value =  "SELECT * FROM stars")
+    @Query("SELECT * FROM stars")
     fun findAll() : List<Star>
+
+    @Query("SELECT * FROM stars WHERE id = :id")
+    fun findstar(id: Int): Flow<Star>
 
     @Query(value =  "SELECT * FROM stars")
     fun readAll() : Flow<List<Star>>
