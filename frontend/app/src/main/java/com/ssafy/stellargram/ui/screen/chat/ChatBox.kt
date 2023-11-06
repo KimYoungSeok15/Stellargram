becd14f43fc54684645bd7f4eedd20fcd71c5799
Sohee-Kim-ln <livre6491@gmail.com>
2023-11-07 08:12:44 +0900

    Feat: 메세지 박스 CSS 적용 및 메세지 입력칸 구현

--- frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/chat/ChatBox.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/chat/ChatBox.kt
@@ -1,47 +1,93 @@
 package com.ssafy.stellargram.ui.screen.chat
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ssafy.stellargram.R
+import com.ssafy.stellargram.ui.theme.Purple80
+import com.ssafy.stellargram.ui.theme.Turquoise
 
 
 @Preview(showBackground = true)
 @Composable
 fun ChatBox(
-    imgUrl : String = "url",
-    nickname : String = "유저의 닉네임",
-    content : String = "메세지 내용"
+    isMine: Boolean = false,
+    imgUrl: String = "url",
+    nickname: String = "유저의 닉네임",
+    content: String = "메세지 내용"
 ) {
-
     val textSize = 22
-    Column(Modifier.padding(10.dp)) {
-        Row {
-            Icon(
-                painter = painterResource(id = R.drawable.account_active),
-                contentDescription = "no Profile Image",
-                Modifier.size(22.dp)
+    val messageBoxCornerSize = 10
+
+    // CSS
+    val containerModifier: Modifier = Modifier
+        .fillMaxWidth()
+        .padding(vertical = 10.dp)
+    val messageBoxModifier: Modifier =
+        Modifier
+            .fillMaxWidth(0.8f)
+            .clip(
+                RoundedCornerShape(
+                    topStart = if (isMine) messageBoxCornerSize.dp else 0.dp,
+                    topEnd = if (isMine) 0.dp else messageBoxCornerSize.dp,
+                    bottomStart = messageBoxCornerSize.dp,
+                    bottomEnd = messageBoxCornerSize.dp
+                )
             )
-            Text(
-                text = nickname,
+            .background(if (isMine) Turquoise else Purple80)
+
+//            .border(
+//                width = 0.dp, color = Color.Black, shape = RoundedCornerShape(10.dp)
+//            )
+            .padding(10.dp)
+
+
+    // 컨테이너 렌더링
+    Column(
+        horizontalAlignment = if (isMine) Alignment.End else Alignment.Start,
+        modifier = containerModifier
+    ) {
+        // 메세지 박스 렌더링
+        Column(
+            modifier = messageBoxModifier
+
+        ) {
+            Row {
+                Icon(
+                    painter = painterResource(id = R.drawable.account_active),
+                    contentDescription = "no Profile Image",
+                    Modifier.size(22.dp)
+                )
+                Text(
+                    text = nickname,
 //                style = TextStyle(fontSize = textSize.sp),
-                modifier = Modifier.padding(top = 4.dp)
+                    modifier = Modifier.padding(top = 4.dp)
+                )
+            }
+            Text(
+                text = content,
+//            style = TextStyle(fontSize = textSize.sp)
             )
         }
-        Text(text = content,
-//            style = TextStyle(fontSize = textSize.sp)
-        )
+
     }
 
 
--- frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/chat/ChatRoomScreen.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/chat/ChatRoomScreen.kt
@@ -1,9 +1,16 @@
 package com.ssafy.stellargram.ui.screen.chat
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,10 +24,32 @@ import androidx.navigation.compose.rememberNavController
 fun ChatRoomScreen(
     navController: NavController = rememberNavController(),
 ) {
-    Column {
-        Text(text = "test", modifier=Modifier.padding(50.dp))
-        ChatBox(nickname = "더하다 우엉차", content = "이 관측소 빛공해는 어느 정도 인가요?")
-        ChatBox(nickname = "쿠크다스", content = "주변에 전광판이 많아서 좀 있는 편이에요")
+    // TODO: 나의 아이디 임시 입력
+    val myId: Int = 1
+    val roomId: Int = 2
 
+    // 입력할 메세지 내용
+    var messageContent by remember { mutableStateOf("") }
+
+    Column {
+        // 메세지들 컨테이너
+        Column {
+            ChatBox(isMine = (myId == 1), nickname = "더하다 우엉차", content = "이 관측소 빛공해는 어느 정도 인가요?")
+            ChatBox(isMine = (myId == 2), nickname = "쿠크다스", content = "주변에 전광판이 많아서 좀 있는 편이에요")
+        }
+
+        // 메세지 입력
+        Row {
+            // 입력 필드
+            TextField(value = messageContent, onValueChange = { messageContent = it })
+            // 전송 버튼
+            Button(onClick = { /*TODO*/ },
+                ) {
+                Text(text = "전송")
+            }
+        }
+
+
+        Text(text = messageContent)
     }
 }
