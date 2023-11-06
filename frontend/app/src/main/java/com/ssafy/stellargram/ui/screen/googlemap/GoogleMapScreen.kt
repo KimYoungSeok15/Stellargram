d03da000c6f68235c4f28a6bebcb8f9bb93ca67e
HidenLee <hyundo1995@gmail.com>
2023-11-06 19:20:07 +0900

    [FE] Feat: GoogleMapScreen Marker Generating with long click

--- frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/googlemap/GoogleMapScreen.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/googlemap/GoogleMapScreen.kt
@@ -1,7 +1,9 @@
 package com.ssafy.stellargram.ui.screen.googlemap
 
 import android.graphics.Color
+import android.util.Log
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
@@ -9,30 +11,31 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.isGranted
 import com.google.accompanist.permissions.rememberPermissionState
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PinConfig
 import com.google.maps.android.compose.AdvancedMarker
 import com.google.maps.android.compose.CameraMoveStartedReason
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
 import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.MarkerInfoWindow
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
+import com.google.maps.android.compose.rememberMarkerState
 import com.google.maps.android.compose.widgets.DisappearingScaleBar
 
 @OptIn(ExperimentalPermissionsApi::class)
@@ -57,10 +60,6 @@ fun GoogleMapScreen(navController: NavController) {
             MapUiSettings(mapToolbarEnabled = false, myLocationButtonEnabled = false)
         )
     }
-
-    // Create a permission launcher
-    val context = LocalContext.current
-    var location by remember { mutableStateOf("Camera location") }
     var lat by remember { mutableStateOf("")}
     var lng by remember { mutableStateOf("")}
     val permissionState = rememberPermissionState(android.Manifest.permission.ACCESS_FINE_LOCATION)
@@ -80,6 +79,7 @@ fun GoogleMapScreen(navController: NavController) {
             lng = String.format("%.5f",nowPosition.longitude)
         }
     }
+    val items = remember { mutableStateListOf<Pair<LatLng, String>>() }
 
     Box(Modifier.fillMaxSize()) {
         GoogleMap(
@@ -87,23 +87,29 @@ fun GoogleMapScreen(navController: NavController) {
             uiSettings = mapUiSettings,
             cameraPositionState = cameraPositionState,
             modifier = Modifier.fillMaxSize(),
-//            onMapLongClick = {latLng -> conten },
+            onMapLongClick = { latLng ->
+                Log.d("IMHERE", "$latLng")
+                Log.d("IMHERE","${items.toList()}")
+                items.add(Pair(latLng,"${latLng.latitude}${latLng.longitude}")) },
             content = {
                 val pinConfig = PinConfig.builder()
                     .setBackgroundColor(Color.MAGENTA)
                     .build()
+                MarkerInfoWindow(state = rememberMarkerState(), onClick = { Log.d("IMHERE", "$it"); true }) { marker ->
+                    Column {
+                        Text(text = marker.title.toString() ?: "Default Marker Title", color = androidx.compose.ui.graphics.Color.Green)
+                        Text(marker.snippet ?: "Default Marker Snippet", color = androidx.compose.ui.graphics.Color.Green)
+                    }
+                }
+                items.forEach {
+//                    AdvancedMarker(
+                    Marker(
+                        state = MarkerState(position = it.first),
+                        title = it.second,
+//                        pinConfig = pinConfig,
 
-                AdvancedMarker(
-                    state = MarkerState(position = LatLng(37.0, 128.0)),
-                    title = "Magenta marker in Sydney",
-                    pinConfig = pinConfig
-                )
-                AdvancedMarker(
-                    state = MarkerState(position = LatLng(37.5, 127.03)),
-                    title = "draggable",
-                    pinConfig = pinConfig,
-                    draggable = true
-                )
+                    )
+                }
             }
         )
         DisappearingScaleBar(
@@ -113,21 +119,9 @@ fun GoogleMapScreen(navController: NavController) {
                 .offset(0.dp, (-50).dp),
             cameraPositionState = cameraPositionState
         )
-        Text(text = "${lat}, ${lng}", modifier = Modifier.align(Alignment.BottomCenter),
+        Text(text = "$lat, $lng", modifier = Modifier.align(Alignment.BottomCenter),
             androidx.compose.ui.graphics.Color.Black)
 
 
-    }
-}
-
-
-
-
-private fun setMapLongClick(map: GoogleMap) {
-    map.setOnMapLongClickListener { latLng ->
-        map.addMarker(
-            MarkerOptions()
-                .position(latLng)
-        )
     }
 }
