a9a571b62ab552faa0375729bbfd2dee7feb0066
GardenJB <ohjb93@gmail.com>
2023-11-06 16:17:28 +0900

    Feat: myId 확인 추가 임시

--- backend/member/src/main/java/com/instargram101/member/api/MemberAspect.java
+++ backend/member/src/main/java/com/instargram101/member/api/MemberAspect.java
@@ -4,10 +4,17 @@ import com.instargram101.global.common.exception.customException.CustomException
 import com.instargram101.member.entity.Member;
 import com.instargram101.member.exception.MemberErrorCode;
 import com.instargram101.member.repoository.MemberRepository;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 
 @Aspect
 @Component
@@ -18,12 +25,25 @@ public class MemberAspect {
         this.memberRepository = memberRepository;
     }
 
-    @Before("execution(* com.instargram101.member.api.MemberController.*.*(..)) && " +
-            " !(execution(* com.instargram101.member.api.MemberController.checkById(..) ) )|| " +
-            " !(execution(* com.instargram101.member.api.MemberController.createMember(..)) )|| " +
-            " !(execution(* com.instargram101.member.api.MemberController.checkByNickname(..)))")
-    public void beforeApiCall(@RequestHeader("myId") Long myId) {
-        Member member = memberRepository.findByMemberIdAndActivated(myId, true)
-                .orElseThrow(() -> new CustomException(MemberErrorCode.Invalid_User));
+    @Before("execution(* com.instargram101.member.api.MemberController.*(..)) && " +
+            "!(execution(* com.instargram101.member.api.MemberController.checkById(..)) || " +
+            "execution(* com.instargram101.member.api.MemberController.createMember(..)) || " +
+            "execution(* com.instargram101.member.api.MemberController.checkByNickname(..)))")
+    public void beforeApiCall(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Method method = methodSignature.getMethod();
+        if(method.isAnnotationPresent(RequestHeader.class)) {
+            RequestHeader requestHeaderAnnotation = method.getAnnotation(RequestHeader.class);
+            String headerName = requestHeaderAnnotation.value();
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String myIdHeader = request.getHeader(headerName);
+
+            if( myIdHeader != null) {
+                Long myId = Long.parseLong(myIdHeader);
+                Member member = memberRepository.findByMemberIdAndActivated(myId, true)
+                        .orElseThrow(() -> new CustomException(MemberErrorCode.Invalid_User));
+            }
+        }
     }
+
 }
