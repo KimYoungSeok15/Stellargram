312d04cd186aba503313a4d5653dbc1cfc23775e
Sohee-Kim-ln <livre6491@gmail.com>
2023-11-08 17:29:01 +0900

    Feat: 랜딩페이지에 채팅방 연결용 임시 버튼 추가

--- frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/landing/LandingScreen.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/landing/LandingScreen.kt
@@ -90,6 +90,16 @@ fun LandingComponent(navController: NavController, modifier: Modifier){
         ) {
             Text(text = "채팅방")
         }
+        // TODO: 채팅방목록 임시 연결버튼. 나중에 지우기
+        Button(
+            onClick = {
+                navController.navigate(Screen.ChatRoomList.route)
+            },
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(text = "채팅방목록")
+        }
     }
 }
 
