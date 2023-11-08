08731c1383a3467529e6a44d0f68aca07da3a43c
Sohee-Kim-ln <livre6491@gmail.com>
2023-11-09 05:18:10 +0900

    Feat: 채팅방 이전 메세지 가져오는 api 추가

--- frontend/app/src/main/java/com/ssafy/stellargram/data/remote/ApiService.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/data/remote/ApiService.kt
@@ -1,17 +1,20 @@
 package com.ssafy.stellargram.data.remote
 
 import com.ssafy.stellargram.model.CardsResponse
+import com.ssafy.stellargram.model.MessageListResponse
 import com.ssafy.stellargram.model.RoomListResponse
 import com.ssafy.stellargram.model.WeatherResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
 
 }
-interface ApiServiceForWeather{
+
+interface ApiServiceForWeather {
     @GET("/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst")
     fun getWeatherData(
         @Query("ServiceKey") serviceKey: String,
@@ -31,17 +34,20 @@ interface ApiServiceForCards {
 }
 
 // 채팅 관련
-interface ApiServiceForChat{
+interface ApiServiceForChat {
     // 내 채팅방 목록 가져오기
     @GET("chat/rooms")
     suspend fun getRoomList(
-        @Header("myId") myId:Int
+        @Header("myId") myId: Int
     ): RoomListResponse
 
-//    // 특정 채팅방의 이전 목록 가져오기
-//    @GET("chat/open/{chatRoomId}/{cursor}")
-//    suspend fun getPrevChats():
-//
-//            //
+    // 특정 채팅방의 이전 메세지 가져오기
+    @GET("chat/open/{chatRoomId}/{cursor}")
+    suspend fun getPrevChats(
+        @Header("myId") myId: Int,
+        @Path("chatRoomId")  chatRoomId:Int,
+        @Path("cursor")  cursor:Int,
+        ): MessageListResponse
+    //
 }
 
