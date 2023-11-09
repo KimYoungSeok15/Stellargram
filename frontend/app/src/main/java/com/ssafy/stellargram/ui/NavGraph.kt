6762ceada117385bcb9f1df076e96951022316fd
이현도 <hyundo1995@naver.com>
2023-11-09 10:15:38 +0900

    Update frontend/app/src/main/java/com/ssafy/stellargram/ui/NavGraph.kt

--- frontend/app/src/main/java/com/ssafy/stellargram/ui/NavGraph.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/ui/NavGraph.kt
@@ -9,14 +9,22 @@ import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.ssafy.stellargram.ui.screen.skymap.SkyMapScreen
+import com.ssafy.stellargram.ui.screen.camera.CameraScreen
+import com.ssafy.stellargram.ui.screen.base.BaseFrame
 import com.ssafy.stellargram.ui.screen.googlemap.GoogleMapScreen
+import com.ssafy.stellargram.ui.screen.home.HomeScreen
 import com.ssafy.stellargram.ui.screen.kakao.KakaoScreen
+import com.ssafy.stellargram.ui.screen.chat.ChatRoomScreen
 import com.ssafy.stellargram.ui.screen.landing.LandingScreen
+import com.ssafy.stellargram.ui.screen.mypage.MypageScreen
+import com.ssafy.stellargram.ui.screen.search.SearchScreen
+import com.ssafy.stellargram.ui.screen.skymap.SkyMapScreen
 
+@androidx.annotation.OptIn(androidx.media3.common.util.UnstableApi::class)
 @Composable
-fun NavGraph(
-    navController: NavHostController = rememberAppNavigationController(),
-    modifier: Modifier = Modifier
+fun  NavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberAppNavigationController()
 ) {
     NavHost(
         navController = navController,
@@ -30,32 +38,38 @@ fun NavGraph(
             KakaoScreen(navController = navController)
         }
         composable(route = Screen.Home.route) {
-            HomeScreen(navController = navController)
+            BaseFrame(navController, screen = Screen.Home) {
+                HomeScreen(navController = navController)
+            }
+        }
+        composable(route = Screen.Search.route) {
+            SearchScreen(navController = navController)
         }
         composable(route = Screen.Example.route) {
             SkyMapScreen(navController = navController, modifier = modifier)
         }
-        composable(route = Screen.GoogleMap.route){
-            GoogleMapScreen(navController = navController)
+        composable(route = Screen.SkyMap.route){
+            BaseFrame(navController, screen = Screen.SkyMap) {
+                SkyMapScreen(navController = navController)
+            }
         }
-
+        composable(route = Screen.Camera.route){
+            BaseFrame(navController, screen = Screen.Camera) {
+                CameraScreen(navController = navController)
+            }
+        }
+        composable(route = Screen.GoogleMap.route){            BaseFrame(navController, screen = Screen.GoogleMap) {
+                GoogleMapScreen(navController = navController)
+            }
+        }
+        composable(route = Screen.MyPage.route){
+            BaseFrame(navController, screen = Screen.MyPage) {
+                MypageScreen(navController = navController)
+            }
+        }
+        composable(route = Screen.ChatRoom.route){
+            ChatRoomScreen(navController = navController)
+        }
     }
 }
-
-
-@Composable
-fun HomeScreen(navController: NavController) {
-    Button(onClick = { navController.navigate(Screen.GoogleMap.route) }) {
-        Text(text = "Google Map")
-    }
-}
-
-//@Composable
-//fun ExampleScreen(navController: NavController) {
-//    Button(onClick = { navController.navigate("Home") }) {
-//        Text(text = "Example")
-//    }
-//}
-
-
 
