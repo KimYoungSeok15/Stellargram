85627f17816d9a8ae5817ed8bd829ba812092ab9
GardenJB <ohjb93@gmail.com>
2023-11-09 15:37:08 +0900

    [FE]Feat: Camera 화면이동 버튼 추가

--- frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/camera/CameraScreen.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/camera/CameraScreen.kt
@@ -1,10 +1,16 @@
 package com.ssafy.stellargram.ui.screen.camera
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -38,46 +44,59 @@ fun GifImage(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-        Button(
-            onClick = {
-                navController.navigate(Screen.Camera.route)
-            },
+        Box(
             modifier = Modifier
                 .padding(16.dp)
-
+                .background(Color.White)
+                .width(400.dp)
+                .height(300.dp)
+                .border(width=1.dp, Color.Black, shape = RoundedCornerShape(20.dp))
+                .clickable { navController.navigate(Screen.Home.route) }
         ) {
-            GlideImage(
-                model = R.drawable.icon_camera,
-                contentDescription = "<a href=\"https://www.flaticon.com/kr/free-animated-icons/\" title=\"카메라 애니메이션 아이콘\">카메라 애니메이션 아이콘 제작자: Freepik - Flaticon</a>",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.size(200.dp)
-            )
-            Text(
-                text = "촬영하기",
-                fontSize = 24.sp,
-                color = Color.Black
-            )
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                GlideImage(
+                    model = R.drawable.icon_camera,
+                    contentDescription = "<a href=\"https://www.flaticon.com/kr/free-animated-icons/\" title=\"카메라 애니메이션 아이콘\">카메라 애니메이션 아이콘 제작자: Freepik - Flaticon</a>",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(200.dp)
+                )
+                Text(
+                    text = "촬영하기",
+                    fontSize = 24.sp,
+                    color = Color.Black,
+                )
+            }
         }
-        Button(
-            onClick = {
-                navController.navigate(Screen.Camera.route)
-            },
+        Box(
             modifier = Modifier
                 .padding(16.dp)
+                .background(Color.White)
+                .width(400.dp)
+                .height(300.dp)
+                .border(width=1.dp, Color.Black, shape = RoundedCornerShape(20.dp))
+                .clickable { navController.navigate(Screen.Home.route) }
         ) {
-            GlideImage(
-                model = R.drawable.icon_gallery,
-                contentDescription = "<a href=\"https://www.flaticon.com/kr/free-animated-icons/\" title=\"그림 애니메이션 아이콘\">그림 애니메이션 아이콘 제작자: Freepik - Flaticon</a>",
-                contentScale = ContentScale.Crop,
-                modifier = Modifier.size(200.dp)
-            )
-            Text(
-                text = "불러오기",
-                fontSize = 24.sp,
-                color = Color.Black
-            )
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                GlideImage(
+                    model = R.drawable.icon_gallery,
+                    contentDescription = "<a href=\"https://www.flaticon.com/kr/free-animated-icons/\" title=\"그림 애니메이션 아이콘\">그림 애니메이션 아이콘 제작자: Freepik - Flaticon</a>",
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier.size(200.dp)
+                )
+                Text(
+                    text = "불러오기",
+                    fontSize = 24.sp,
+                    color = Color.Black,
+                )
+            }
         }
-
     }
-
 }
