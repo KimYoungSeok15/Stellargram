e11e414e18b283f14ba2dd1c563583247968f486
GardenJB <ohjb93@gmail.com>
2023-11-14 14:16:45 +0900

    [BE]Fix: 멤버 정보 조회 시 팔로우 여부 포함

--- backend/member/src/main/java/com/instargram101/member/service/FollowService.java
+++ backend/member/src/main/java/com/instargram101/member/service/FollowService.java
@@ -1,10 +1,13 @@
 package com.instargram101.member.service;
 
+import com.instargram101.member.dto.response.FindMemberResponseDto;
 import com.instargram101.member.entity.Member;
 
 import java.util.List;
 
 public interface FollowService {
+    FindMemberResponseDto findFollow(Long follower, Long followee);
+    List<FindMemberResponseDto> findMembers(Long myId, List<Member> memberList);
     Boolean followUser(Long follower, Long followee);
     Boolean deleteFollow(Long followerId, Long followeeId);
     void setFollowingCount(Member member, int count);
