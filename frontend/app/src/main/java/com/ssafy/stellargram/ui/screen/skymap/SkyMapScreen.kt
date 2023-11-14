107431abfd18d33460a2a0a63d3fe7f9093d459b
HyundoLee <hyundo1995@gmail.com>
2023-11-14 16:43:03 +0900

    [FE] Feat: get Device orientation for rendering planetarium

--- frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/skymap/SkyMapScreen.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/ui/screen/skymap/SkyMapScreen.kt
@@ -1,10 +1,12 @@
 package com.ssafy.stellargram.ui.screen.skymap
 
+import android.app.Activity
+import android.hardware.Sensor
+import android.hardware.SensorManager
 import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.detectTransformGestures
@@ -20,8 +22,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Slider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -34,19 +38,28 @@ import androidx.compose.ui.graphics.TileMode
 import androidx.compose.ui.graphics.drawscope.DrawStyle
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.rotationMatrix
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.ssafy.stellargram.module.DBModule
 import com.ssafy.stellargram.module.ScreenModule
+import com.ssafy.stellargram.ui.MainActivity
 import com.ssafy.stellargram.util.ConstellationLine
 import com.ssafy.stellargram.util.Temperature
+import dev.ricknout.composesensors.getSensor
+import dev.ricknout.composesensors.getSensorManager
+import dev.ricknout.composesensors.gyroscope.rememberGyroscopeSensorValueAsState
+import dev.ricknout.composesensors.rememberSensorValueAsState
 import kotlinx.coroutines.delay
 import java.lang.Math.log
 import java.lang.Math.pow
 import java.lang.Math.sqrt
+import java.text.DecimalFormat
 import kotlin.math.pow
+import kotlin.math.roundToInt
 import kotlin.random.Random
 
 
@@ -57,6 +70,10 @@ fun SkyMapScreen(navController : NavController){
     val viewModel : SkyMapViewModel = viewModel()
     val temperature: Temperature = Temperature()
     val constellationLine: ConstellationLine = ConstellationLine()
+
+
+
+
 
     // TODO: 기기로부터 정보를 받는 거로 고쳐야 함.
     val longitude: Double = 127.039611
@@ -119,6 +136,22 @@ fun SkyMapScreen(navController : NavController){
 //        constSight = viewModel.getAllConstellations(longitude, latitude, zoom.toDouble(), theta, phi, screenHeight.toDouble(), screenWidth.toDouble())
 //    }
 
+    var orientationValues by remember { mutableStateOf(Triple(0f, 0f, 0f)) }
+    val context = LocalContext.current
+    val orientation = remember { Orientation(context as Activity) }
+    DisposableEffect(context) {
+        orientation.startListening(object : Orientation.Listener {
+            override fun onOrientationChanged(pitch: Float, roll: Float, yaw: Float) {
+                orientationValues = Triple(pitch, roll, yaw)
+                theta = -yaw.toDouble()
+                phi = pitch.toDouble()
+            }
+        })
+        onDispose {
+            orientation.stopListening()
+        }
+    }
+
     Box(){
         Canvas(
             modifier = Modifier
@@ -127,7 +160,7 @@ fun SkyMapScreen(navController : NavController){
                     color = Color.Black
                 )
                 .transformable(rememberTransformableState { zoomChange: Float, panChange: Offset, rotationChange: Float ->
-                    if(zoom*zoomChange in  0.7f .. 10f){
+                    if (zoom * zoomChange in 0.7f..10f) {
                         zoom = (zoom * zoomChange)
                     }
                     theta -= (panChange.x * zoom) / 10
@@ -250,6 +283,10 @@ fun SkyMapScreen(navController : NavController){
                 .align(Alignment.TopStart)
                 .padding(16.dp)
         ){
+            Text(text = "${orientationValues.first}")
+            Text(text = "${orientationValues.second}")
+            Text(text = "${orientationValues.third}")
+
             if(clicked) Text("${nameMap[clickedIndex]}",
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
