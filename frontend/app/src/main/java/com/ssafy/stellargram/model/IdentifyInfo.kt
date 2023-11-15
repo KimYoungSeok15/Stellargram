e3c8722cf31a9e40f774dd1a5a7afdf3d2b20a09
Sohee-Kim-ln <livre6491@gmail.com>
2023-11-16 07:28:01 +0900

    Feat: 사진 인식 응답용 dto 수정 및 데이터 담을 dto 분리

--- frontend/app/src/main/java/com/ssafy/stellargram/model/IdentifyInfo.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/model/IdentifyInfo.kt
@@ -1,8 +1,14 @@
 package com.ssafy.stellargram.model
 
+// 인식 응답
+data class IdentifyResponse(
+    val code: Int,
+    val message: String,
+    val data: MessagesData
+)
 
 // 사진 1개에 대해 인식된 정보
-data class IdentifyResponse(
+data class IdentifyPhotoInfo (
     var Dec: Long?,// 사진 정가운데의 적위
     var FOV: Long?,
     var Matches: Int?, //매칭된 별 갯수
@@ -13,20 +19,18 @@ data class IdentifyResponse(
     var T_extract: Long, // 별 검출 소요시간
     var T_solve: Long, // 별 인식 소요시간
     var distortion: Long?,
-    var matched: List<IdentifyStarInfo>?,
-    var matched_catID: List<Int>?
+    var matched: List<IdentifyStarInfo>?, // 인식된 별들의 정보
+    var matched_catID: List<Int>? // 인식된 별들의 hip 아이디
+
 )
 
-//data class IdentifyPhotoInfo (
-//
-//)
-
+// TODO: 백 응답 수정되면 수정할 것
 // 인식된 별 1개 정보
 data class IdentifyStarInfo(
-    var absmag: Long,
+    var absmag: Long, // 절대등급
     var con: String,
-    var id: Int,
-    var mag: Long,
+    var id: Int, // db내 
+    var mag: Long, // 등급
     var pixelx: Int,
     var pixely: Int
 )
