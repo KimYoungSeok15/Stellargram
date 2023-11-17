4470399266ac8aac6311d98f2981f3b04f087b23
HidenLee <hyundo1995@gmail.com>
2023-11-17 13:56:43 +0900

    [FE] HOTFIX: Remove redeclaration interface and model

--- frontend/app/src/main/java/com/ssafy/stellargram/data/remote/ApiService.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/data/remote/ApiService.kt
@@ -1,27 +1,14 @@
 package com.ssafy.stellargram.data.remote
 
-import com.ssafy.stellargram.model.CardsResponse
-import com.ssafy.stellargram.model.CursorResponse
-import com.ssafy.stellargram.model.MessageListResponse
-import com.ssafy.stellargram.model.RoomListResponse
 import com.ssafy.stellargram.model.MemberCheckDuplicateRequest
 import com.ssafy.stellargram.model.MemberCheckDuplicateResponse
 import com.ssafy.stellargram.model.MemberCheckResponse
 import com.ssafy.stellargram.model.MemberSignUpRequest
 import com.ssafy.stellargram.model.MemberSignUpResponse
-import com.ssafy.stellargram.model.ObserveSiteListResponse
-import com.ssafy.stellargram.model.ObserveSiteRequest
-import com.ssafy.stellargram.model.ObserveSiteResponse
-import com.ssafy.stellargram.model.SiteInfoResponse
-import com.ssafy.stellargram.model.WeatherResponse
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
-import retrofit2.http.Path
 import retrofit2.http.POST
-import retrofit2.http.Query
 
 interface ApiService {
      @GET("member/check")
@@ -33,78 +20,3 @@ interface ApiService {
      @POST("member/signup")
      suspend fun postMemberSignUP(@Body postMemberSignUpRequest : MemberSignUpRequest) : Response<MemberSignUpResponse>
 }
-
-interface ApiServiceForWeather{
-    @GET("/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst")
-    fun getWeatherData(
-        @Query("ServiceKey") serviceKey: String,
-        @Query("pageNo") pageNo: Int,
-        @Query("numOfRows") numOfRows: Int,
-        @Query("dataType") dataType: String,
-        @Query("base_date") baseDate: String,
-        @Query("base_time") baseTime: String,
-        @Query("nx") nx: Int,
-        @Query("ny") ny: Int
-    ): Call<WeatherResponse>
-}
-
-interface ApiServiceForCards {
-    @GET("/starcard")
-    suspend fun getCards(): CardsResponse
-}
-
-// 채팅 관련
-interface ApiServiceForChat {
-    // 내 채팅방 목록 가져오기
-    @GET("chat/rooms")
-    suspend fun getRoomList(
-        @Header("myId") myId: Long
-    ): RoomListResponse
-
-    // 특정 채팅방의 이전 메세지 가져오기
-    @GET("chat/open/{chatRoomId}/{cursor}")
-    suspend fun getPrevChats(
-        @Header("myId") myId: Long,
-        @Path("chatRoomId") chatRoomId: Int,
-        @Path("cursor") cursor: Int,
-    ): MessageListResponse
-
-    // 특정 채팅방의 가장 마지막 커서 가져오기
-    @GET("chat/recentCurser/{chatRoomId}")
-    suspend fun getRecentCursor(
-        @Path("chatRoomId") chatRoomId: Int,
-    ): CursorResponse
-}
-
-interface ApiServiceForObserveSite {
-
-    //관측지 post
-    @POST("observe-site/")
-    suspend fun postObserveSite(
-        @Body observeSiteRequest: ObserveSiteRequest
-    ): ObserveSiteResponse
-}
-
-interface ApiServiceForObserveSearch {
-    @GET("observe-search/")
-    suspend fun getObserveSearch(
-        @Query("startX") startX: Float,
-        @Query("endX") endX: Float,
-        @Query("startY") startY: Float,
-        @Query("endY") endY: Float
-    ): ObserveSiteListResponse
-}
-
-// 관측포인트 관련
-interface ApiServiceForSite {
-
-    // 관측포인트 상세 조회
-    @GET("observe-site/latitude/{latitude}/longitude/{longitude}")
-    suspend fun getSiteInfo(
-//        @Header("myId") myId: Long,
-        @Path("latitude") latitude: Double,
-        @Path("longitude") longitude: Double,
-    ): SiteInfoResponse
-
-}
-
--- frontend/app/src/main/java/com/ssafy/stellargram/model/ObserveSite.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/model/ObserveSite.kt
@@ -13,27 +13,3 @@ data class SiteInfoResponse(
     val message: String,
     val data: SiteInfo
 )
-
-data class ObserveSiteResponse (
-    val code: Int,
-    val message: String,
-    val data: ObserveSite
-)
-
-data class ObserveSiteListResponse(
-    val code: Int,
-    val message: String,
-    val data: MutableList<ObserveSite>
-)
-data class ObserveSite(
-    val latitude: Float,
-    val longitude: Float,
-    val name: String,
-    val reviewCount: Int,
-    val ratingSum: Int
-)
-data class ObserveSiteRequest(
-    val latitude: Double,
-    val longitude: Double,
-    val name: String
-)
