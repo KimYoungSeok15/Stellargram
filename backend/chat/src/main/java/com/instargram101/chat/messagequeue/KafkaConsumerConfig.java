e1f1b5e4a87c9ba7dc17c264a381d07642aada0d
허재 <huhwo00@gmail.com>
2023-11-18 20:44:17 +0900

    Chore: 카프카 연결 주소 수정

--- backend/chat/src/main/java/com/instargram101/chat/messagequeue/KafkaConsumerConfig.java
+++ backend/chat/src/main/java/com/instargram101/chat/messagequeue/KafkaConsumerConfig.java
@@ -20,8 +20,8 @@ public class KafkaConsumerConfig {
     public ConsumerFactory<String, String> consumerFactory(){
         Map<String, Object> properties = new HashMap<>();
 
-//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "k9a101.p.ssafy.io:9092");
-        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "k9a101.p.ssafy.io:9092");
+//        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
         properties.put(ConsumerConfig.GROUP_ID_CONFIG, "consumerGroupId2");
         properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
