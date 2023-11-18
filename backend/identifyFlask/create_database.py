f2b0f832a3e1fca1ffc120ee23b7af241f265606
Sohee-Kim-ln <livre6491@gmail.com>
2023-11-18 12:06:36 +0900

    Hotfix: 테트라DB 생성을 5등급으로 롤백, 최저화각 0.1에서 1로 변경

--- backend/identifyFlask/create_database.py
+++ backend/identifyFlask/create_database.py
@@ -1,4 +1,4 @@
 import tetra3
 
 t3 = tetra3.Tetra3()
-t3.generate_database(max_fov=50, min_fov=0.1, star_max_magnitude=7, save_as='default_database_custom_5_max50')
+t3.generate_database(max_fov=50, min_fov=1, star_max_magnitude=5, save_as='default_database_custom_5_max50')
