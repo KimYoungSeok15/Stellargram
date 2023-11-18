e1c26bb47bbd4d63267d709ec4e22ddbeb42dd3b
Sohee-Kim-ln <livre6491@gmail.com>
2023-11-18 17:40:49 +0900

    Feat: 표시를 위해 관측소 정보 조회해서 담을 데이터 dto 추가

--- frontend/app/src/main/java/com/ssafy/stellargram/model/ChatRoom.kt
+++ frontend/app/src/main/java/com/ssafy/stellargram/model/ChatRoom.kt
@@ -1,18 +1,31 @@
 package com.ssafy.stellargram.model
 
+// 채팅방 1개 정보
 data class ChatRoom(
     val roomId: Int,
     val personnel: Int,
     val observeSiteId: String
 )
 
+// 채팅방 리스트 응답 시 data
 data class ChatRoomsData(
-    val roomCount:Int,
+    val roomCount: Int,
     val roomList: List<ChatRoom>
 )
 
+// 채팅방 리스트 응답용
 data class RoomListResponse(
     val code: Int,
     val message: String,
     val data: ChatRoomsData
+)
+
+// 채팅방 1개 정보 + 해당 관측소 데이터 조회 정보
+data class CombinedChatRoom(
+    val roomId: Int,
+    val personnel: Int,
+    val observeSiteId: String,
+    val siteName: String,
+    val longitude: Long,
+    val latitude: Long
 )
