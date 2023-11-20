8ae4006252f561cda08d7ff55d040c37037e091f
Sohee-Kim-ln <livre6491@gmail.com>
2023-11-20 09:36:52 +0900

    Hotfix: 채팅방 한계 설정 수정

--- backend/chat/src/main/java/com/instargram101/chat/config/ChattingConfig.java
+++ backend/chat/src/main/java/com/instargram101/chat/config/ChattingConfig.java
@@ -8,11 +8,11 @@ import org.springframework.context.annotation.Configuration;
 public class ChattingConfig {
 
     // 최대 채팅방 수
-    private final int maxRoomCount = 5;
+    private final int maxRoomCount = 100;
 
     // 채팅방 별 최대 인원 수
     private final int maxPersonnelCount = 100;
 
     // 페이지네이션 최대 갯수
-    private final int pageSize = 5;
+    private final int pageSize = 10;
 }
